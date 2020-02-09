698c86bf3151530c05690d24315069d5049ad785
Mikaru <1183970003@qq.com>
2020-02-09 15:26:06 +0800

    first commit

---
+++ Cluster_2/Caves/modoverrides.lua
@@ -0,0 +1,90 @@
+return {
+  ["workshop-1125385407"]={ configuration_options={ difficulty=2, language=2 }, enabled=true },
+  ["workshop-1173770304"]={ configuration_options={ language=2 }, enabled=true },
+  ["workshop-1271089343"]={
+    configuration_options={ bekkitt_skeleton_pen=0, null_option=true, skeleton_player=true },
+    enabled=true 
+  },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-371920345"]={ configuration_options={  }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=3,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=true,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=3 
+    },
+    enabled=true 
+  },
+  ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-543945797"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="bouncy",
+      dmg_only="on",
+      number_size="default",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-661253977"]={
+    configuration_options={ amudiao=true, baodiao=0, kong=0, rendiao=0, zbdiao=false },
+    enabled=true 
+  },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      food_estimation=-1,
+      food_order=1,
+      food_style=1,
+      lang="chs",
+      show_food_units=0,
+      show_uses=1 
+    },
+    enabled=true 
+  },
+  ["workshop-767776640"]={
+    configuration_options={
+      bonus=0,
+      maxweapon=1,
+      null=1,
+      overmaxarmor=false,
+      overmaxfiniteuses=false,
+      overmaxfueled=false,
+      overmaxperishable=false,
+      repairarmor=true,
+      repairfiniteuses=true,
+      repairfueled=true,
+      repairperishable=true 
+    },
+    enabled=true 
+  },
+  ["workshop-770901818"]={
+    configuration_options={ days=2, enable_houndattack=true, format="complex" },
+    enabled=true 
+  },
+  ["workshop-829829879"]={ configuration_options={ Language=true, Sound=true }, enabled=true } 
+}
