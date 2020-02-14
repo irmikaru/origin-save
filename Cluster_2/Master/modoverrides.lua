36576af98ffefa6e6eab4743065aab86520ba380
Mikaru <1183970003@qq.com>
2020-02-14 13:16:45 +0800

    2020/2/14

--- Cluster_2/Master/modoverrides.lua
+++ Cluster_2/Master/modoverrides.lua
@@ -1,8 +1,110 @@
 return {
-  ["workshop-1125385407"]={ configuration_options={ difficulty=2, language=2 }, enabled=true },
-  ["workshop-1173770304"]={ configuration_options={ language=2 }, enabled=true },
+  ["workshop-1085586145"]={
+    configuration_options={
+      additional_building="open",
+      additional_chesspieces="open",
+      additional_dress="open",
+      additional_experiment="open",
+      additional_food="open",
+      additional_magic="open",
+      additional_orbit="open",
+      additional_survival="open",
+      additional_weapon="open",
+      dev_mode="disabled",
+      dress_uses="normal",
+      food_effect="normal",
+      language="english",
+      survival_effect="normal",
+      tooltip_enhance="open",
+      weapon_damage="normal",
+      weapon_uses="normal" 
+    },
+    enabled=true 
+  },
+  ["workshop-1153998909"]={
+    configuration_options={
+      [""]=0,
+      ARMOR_BONE_BLOCK_VALUE=0.8,
+      ARMOR_BONE_DURABILITY_MULTIPLICATOR=5,
+      ARMOR_BONE_INGREDIENT_BONES=6,
+      ARMOR_BONE_INGREDIENT_ROPE=2,
+      ARMOR_STONE_BLOCK_VALUE=0.8,
+      ARMOR_STONE_DURABILITY_MULTIPLICATOR=2.5,
+      ARMOR_STONE_INGREDIENT_ROCKS=6,
+      ARMOR_STONE_INGREDIENT_ROPE=2,
+      ["BONE SUIT CONFIG:"]=0,
+      ["STONE SUIT CONFIG:"]=0 
+    },
+    enabled=true 
+  },
+  ["workshop-1234341720"]={
+    configuration_options={
+      battleaxe="normal",
+      book_dogs="normal",
+      book_freeze="normal",
+      book_healing="normal",
+      book_lava="normal",
+      book_light="normal",
+      book_meteor="normal",
+      book_sleepingfield="normal",
+      book_slowness="normal",
+      book_summon="normal",
+      book_tornado="normal",
+      deathscythe="normal",
+      flamesword="normal",
+      halberd="normal",
+      healingstaff2="normal",
+      katana="normal",
+      lightningsword="normal",
+      pirate="normal",
+      poseidon="normal",
+      purplesword="normal",
+      skullspear="normal",
+      teleportstaff="normal" 
+    },
+    enabled=true 
+  },
   ["workshop-1271089343"]={
     configuration_options={ bekkitt_skeleton_pen=0, null_option=true, skeleton_player=true },
+    enabled=true 
+  },
+  ["workshop-1299651123"]={ configuration_options={  }, enabled=true },
+  ["workshop-1365023186"]={ configuration_options={  }, enabled=true },
+  ["workshop-1712895405"]={
+    configuration_options={
+      ANTIDOTE_DURABILITY=20,
+      ANTIDOTE_RECIPE_GLANDS=10,
+      ANTIDOTE_RECIPE_NITRE=0,
+      ANTIDOTE_RECIPE_PHLEGM=1 
+    },
+    enabled=true 
+  },
+  ["workshop-1750657150"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="bouncy",
+      dmg_only="on",
+      number_size="default",
+      show_beaverness="off",
+      show_decimal_points="no",
+      show_fueled="off",
+      show_hunger="off",
+      show_moisture="off",
+      show_sanity="off",
+      show_work="off",
+      size_scale="default" 
+    },
+    enabled=true 
+  },
+  ["workshop-1935547247"]={
+    configuration_options={
+      CleverHand=true,
+      Language=true,
+      NightVision=false,
+      snowball_cost=3,
+      snowball_num=1,
+      snowball_pvp=false 
+    },
     enabled=true 
   },
   ["workshop-347079953"]={
@@ -36,18 +138,10 @@ return {
     },
     enabled=true 
   },
+  ["workshop-418604101"]={ configuration_options={  }, enabled=true },
   ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
   ["workshop-466732225"]={ configuration_options={  }, enabled=true },
-  ["workshop-543945797"]={
-    configuration_options={
-      amount_of_numbers="default",
-      display_mode="bouncy",
-      dmg_only="on",
-      number_size="default",
-      show_decimal_points="no" 
-    },
-    enabled=true 
-  },
+  ["workshop-493673961"]={ configuration_options={ key4=122 }, enabled=true },
   ["workshop-661253977"]={
     configuration_options={ amudiao=true, baodiao=0, kong=0, rendiao=0, zbdiao=false },
     enabled=true 
@@ -86,5 +180,6 @@ return {
     configuration_options={ days=2, enable_houndattack=true, format="complex" },
     enabled=true 
   },
-  ["workshop-829829879"]={ configuration_options={ Language=true, Sound=true }, enabled=true } 
+  ["workshop-818739975"]={ configuration_options={ DFV_Language="CN" }, enabled=true },
+  ["workshop-934638020"]={ configuration_options={  }, enabled=true } 
 }
---
+++ history/Cluster_2/Master/modoverrides.lua
@@ -0,0 +1,178 @@
+return {
+  ["workshop-1085586145"]={
+    configuration_options={
+      additional_building="open",
+      additional_chesspieces="open",
+      additional_dress="open",
+      additional_experiment="open",
+      additional_food="open",
+      additional_magic="open",
+      additional_orbit="open",
+      additional_survival="open",
+      additional_weapon="open",
+      dev_mode="disabled",
+      dress_uses="normal",
+      food_effect="normal",
+      language="english",
+      survival_effect="normal",
+      tooltip_enhance="open",
+      weapon_damage="normal",
+      weapon_uses="normal" 
+    },
+    enabled=true 
+  },
+  ["workshop-1125385407"]={ configuration_options={ difficulty=2, language=2 }, enabled=true },
+  ["workshop-1153998909"]={
+    configuration_options={
+      [""]=0,
+      ARMOR_BONE_BLOCK_VALUE=0.8,
+      ARMOR_BONE_DURABILITY_MULTIPLICATOR=5,
+      ARMOR_BONE_INGREDIENT_BONES=6,
+      ARMOR_BONE_INGREDIENT_ROPE=2,
+      ARMOR_STONE_BLOCK_VALUE=0.8,
+      ARMOR_STONE_DURABILITY_MULTIPLICATOR=2.5,
+      ARMOR_STONE_INGREDIENT_ROCKS=6,
+      ARMOR_STONE_INGREDIENT_ROPE=2,
+      ["BONE SUIT CONFIG:"]=0,
+      ["STONE SUIT CONFIG:"]=0 
+    },
+    enabled=true 
+  },
+  ["workshop-1173770304"]={ configuration_options={ language=2 }, enabled=true },
+  ["workshop-1234341720"]={
+    configuration_options={
+      battleaxe="normal",
+      book_dogs="normal",
+      book_freeze="normal",
+      book_healing="normal",
+      book_lava="normal",
+      book_light="normal",
+      book_meteor="normal",
+      book_sleepingfield="normal",
+      book_slowness="normal",
+      book_summon="normal",
+      book_tornado="normal",
+      deathscythe="normal",
+      flamesword="normal",
+      halberd="normal",
+      healingstaff2="normal",
+      katana="normal",
+      lightningsword="normal",
+      pirate="normal",
+      poseidon="normal",
+      purplesword="normal",
+      skullspear="normal",
+      teleportstaff="normal" 
+    },
+    enabled=true 
+  },
+  ["workshop-1271089343"]={
+    configuration_options={ bekkitt_skeleton_pen=0, null_option=true, skeleton_player=true },
+    enabled=true 
+  },
+  ["workshop-1299651123"]={ configuration_options={  }, enabled=true },
+  ["workshop-1426323391"]={
+    configuration_options={
+      izayoi_se=true,
+      izayoi_voice=true,
+      recipes="normal",
+      strength="normal",
+      watch_craftable=true,
+      watch_food_spoilage=0.75,
+      watch_night_vision=false,
+      watch_players_equippable=true,
+      x_hostile_only=false,
+      z_escape=true 
+    },
+    enabled=true 
+  },
+  ["workshop-1837053004"]={
+    configuration_options={ compatible=false, elite=false, language="AUTO", playerdmg=true, skillkey=108 },
+    enabled=true 
+  },
+  ["workshop-1851574434"]={ configuration_options={  }, enabled=true },
+  ["workshop-1861727307"]={ configuration_options={  }, enabled=true },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-371920345"]={ configuration_options={  }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=3,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=true,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=3 
+    },
+    enabled=true 
+  },
+  ["workshop-418604101"]={ configuration_options={  }, enabled=true },
+  ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-493673961"]={ configuration_options={ key4=122 }, enabled=true },
+  ["workshop-543945797"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="bouncy",
+      dmg_only="on",
+      number_size="default",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-661253977"]={
+    configuration_options={ amudiao=true, baodiao=0, kong=0, rendiao=0, zbdiao=false },
+    enabled=true 
+  },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      food_estimation=-1,
+      food_order=1,
+      food_style=1,
+      lang="chs",
+      show_food_units=0,
+      show_uses=1 
+    },
+    enabled=true 
+  },
+  ["workshop-767776640"]={
+    configuration_options={
+      bonus=0,
+      maxweapon=1,
+      null=1,
+      overmaxarmor=false,
+      overmaxfiniteuses=false,
+      overmaxfueled=false,
+      overmaxperishable=false,
+      repairarmor=true,
+      repairfiniteuses=true,
+      repairfueled=true,
+      repairperishable=true 
+    },
+    enabled=true 
+  },
+  ["workshop-770901818"]={
+    configuration_options={ days=2, enable_houndattack=true, format="complex" },
+    enabled=true 
+  } 
+}
